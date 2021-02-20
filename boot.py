f8b4ef5ff251e5ead387165d730966655560ebea
xXEthyleneXx <xXEthyleneXx@protonmail.com>
2021-02-19 20:13:42 -0500

    `

--- boot.py
+++ boot.py
@@ -9,17 +9,34 @@ def main():
         def updater():
             home = os.path.expanduser("~")
             os.chdir(home)
-            dlurl = "https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/update.py"
-            path = str(home) + "/update.pyw"
-            try:
-                os.remove('update.pyw')
-            except FileNotFoundError:
-                print('Its Not Here')
+            dlurl = "https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/u.txt"
+            path = str(home) + "/u.txt"
+            #try:
+                #os.remove('u.txt')
+            #except FileNotFoundError:
+                #print('Its Not Here')
             urllib.request.urlretrieve(dlurl, '{}'.format(path))
-            #os.system("start cmd /c {}".format('pyw ' + path))
-            sleep(.2)
-            subprocess.Popen(['pyw', 'update.pyw'])
-            exit()
+            U = open('u.txt', 'r')
+            update = int(U.readline(1))
+            U.close()
+            os.remove('u.txt')
+            if update == 0:
+                home = os.path.expanduser("~")
+                os.chdir(home)
+                dlurl = "https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/update.py"
+                path = str(home) + "/update.pyw"
+                try:
+                    os.remove('update.pyw')
+                except FileNotFoundError:
+                    print('Its Not Here')
+                urllib.request.urlretrieve(dlurl, '{}'.format(path))
+                #os.system("start cmd /c {}".format('pyw ' + path))
+                subprocess.Popen(['pyw', 'update.pyw'])
+                exit()
+            else:
+                print('Fully Up-To-Date')
+                V = 0
+                return V
 
 
         def trolls():
@@ -30,7 +47,7 @@ def main():
             return T
 
         if V == 10:
-            updater()
+            V = updater()
         else:
             trolls()
             Time = rnumg()
